d9ee62c62783c4508f44dde7b2e30e4e2546cf04
Juan I. Carlis <juancarlis93@gmail.com>
2021-08-05 23:40:06 -0300

    correcting api_key

--- testing-api.py
+++ testing-api.py
@@ -1,4 +1,10 @@
 import requests
 
+
+API_KEY = ''
+
 response = requests.get(
-    'https://calendarific.com/api/v2/holidays?api_key=87d04b7e567fbef95171bbde43d3c320c6cbc2e2&country=AR&year=2021')
+    f'https://calendarific.com/api/v2/holidays?api_key={API_KEY}&country={country}&year={year}')
+
+
+print(response)
